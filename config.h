4b0b19dbe238e8743a982c68e275f9375fd5b1d9
Robert Hayden <7975733+f-eq-ma@users.noreply.github.com>
2021-09-04 15:28:10 -0500

    Uploaded changes to support tap dance for F9 and F10 keys

--- config.h
+++ config.h
@@ -1,4 +1,4 @@
-/* Copyright 2021 Robert Hayden
+/* Copyright 2019 Louwii
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,3 +17,4 @@
 #pragma once
 
 // place overrides here
+#define TAPPING_TERM 200
--- keymap.c
+++ keymap.c
@@ -25,23 +25,53 @@
 //   scroll lock -> KC_VOLU
 //   print screen -> F14 (AHK controlled - iTunes Back)
 //
+// Tap Dance:
+// F9 -> f9 | (CWXTITVAL-)
+// F10 -> f10 | {noformat}
+//
+
 #include QMK_KEYBOARD_H
 
-// Defines the keycodes used by our macros in process_record_user
-enum custom_keycodes {
-  QMKBEST = SAFE_RANGE,
-  QMKURL
+enum {
+  TD_F10_NOFORMAT,
+  TD_F9_CWXTITVAL
 };
+
+void td_f10_noformat_func(qk_tap_dance_state_t *state, void *user_data) {
+	if (state->count == 1) {
+		register_code16(KC_F10);
+		unregister_code16(KC_F10);
+	} else {
+		SEND_STRING("{noformat}");
+		reset_tap_dance(state);
+	}
+}
+
+void td_f9_cwxtitval_func(qk_tap_dance_state_t *state, void *user_data) {
+	if (state->count == 1) {
+		register_code16(KC_F9);
+		unregister_code16(KC_F9);
+	} else {
+		SEND_STRING(" (CWXTITVAL-)");
+		reset_tap_dance(state);
+	}
+}
+
+//Tap Dance definitions
+qk_tap_dance_action_t tap_dance_actions[] = {
+	[TD_F9_CWXTITVAL] = ACTION_TAP_DANCE_FN(td_f9_cwxtitval_func),
+	[TD_F10_NOFORMAT] = ACTION_TAP_DANCE_FN(td_f10_noformat_func)
+}; // comment
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_tkl_ansi(
-      KC_ESC,  KC_F1,  KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8,  KC_F9,  KC_F10, KC_F11,  KC_F12,                      KC_F16,  KC_F17,  KC_F19,
-      KC_GRV,  KC_1,   KC_2,  KC_3,  KC_4,  KC_5,  KC_6,  KC_7,  KC_8,   KC_9,   KC_0,   KC_MINS, KC_EQL,  KC_BSPC,            KC_INS,  KC_HOME, KC_PGUP,
-      KC_TAB,  KC_Q,   KC_W,  KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,  KC_I,   KC_O,   KC_P,   KC_LBRC, KC_RBRC, KC_BSLS,            KC_DEL,  KC_END,  KC_PGDN,
-      KC_CAPS, KC_A,   KC_S,  KC_D,  KC_F,  KC_G,  KC_H,  KC_J,  KC_K,   KC_L,   KC_SCLN,KC_QUOT,          KC_ENT,                                       
-      KC_LSFT, KC_Z,   KC_X,  KC_C,  KC_V,  KC_B,  KC_N,  KC_M,  KC_COMM,KC_DOT, KC_SLSH,                  KC_RSFT,                     KC_UP,           
-      KC_LCTL, KC_LGUI,KC_LALT,                     KC_SPC,                      KC_RALT,MO(1),   KC_APP,  KC_RCTL,            KC_LEFT, KC_DOWN, KC_RGHT   ),
+      KC_ESC,  KC_F1,  KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8,  TD(TD_F9_CWXTITVAL), TD(TD_F10_NOFORMAT), KC_F11,  KC_F12,                      KC_F16,  KC_F17,  KC_F19,
+      KC_GRV,  KC_1,   KC_2,  KC_3,  KC_4,  KC_5,  KC_6,  KC_7,  KC_8,   KC_9,                KC_0,                KC_MINS, KC_EQL,  KC_BSPC,            KC_INS,  KC_HOME, KC_PGUP,
+      KC_TAB,  KC_Q,   KC_W,  KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,  KC_I,   KC_O,                KC_P,                KC_LBRC, KC_RBRC, KC_BSLS,            KC_DEL,  KC_END,  KC_PGDN,
+      KC_CAPS, KC_A,   KC_S,  KC_D,  KC_F,  KC_G,  KC_H,  KC_J,  KC_K,   KC_L,                KC_SCLN,             KC_QUOT, KC_ENT,                                       
+      KC_LSFT, KC_Z,   KC_X,  KC_C,  KC_V,  KC_B,  KC_N,  KC_M,  KC_COMM,KC_DOT,              KC_SLSH,                  KC_RSFT,                         KC_UP,           
+      KC_LCTL, KC_LGUI,KC_LALT,                     KC_SPC,                                   KC_RALT,             MO(1),   KC_APP,  KC_RCTL,            KC_LEFT, KC_DOWN, KC_RGHT   ),
   [1] = LAYOUT_tkl_ansi(
       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,           KC_VOLD, KC_VOLU, KC_F14,
       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,
@@ -50,28 +80,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS,           KC_TRNS,         
       KC_TRNS, KC_TRNS, KC_TRNS,                          KC_TRNS,                              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS   )
 };
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-    case QMKBEST:
-      if (record->event.pressed) {
-        // when keycode QMKBEST is pressed
-        SEND_STRING("QMK is the best thing ever!");
-      } else {
-        // when keycode QMKBEST is released
-      }
-      break;
-    case QMKURL:
-      if (record->event.pressed) {
-        // when keycode QMKURL is pressed
-        SEND_STRING("https://qmk.fm/" SS_TAP(X_ENTER));
-      } else {
-        // when keycode QMKURL is released
-      }
-      break;
-  }
-  return true;
-}
 
 void matrix_init_user(void) {
 
