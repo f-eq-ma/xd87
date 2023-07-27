09f0acbc28d83d76a691eccdd0dbbe5eed95cdd1
Robert Hayden <7975733+f-eq-ma@users.noreply.github.com>
2023-07-26 21:10:46 -0500

    Update keymap.c

--- keymap.c
+++ keymap.c
@@ -33,9 +33,32 @@
 #include QMK_KEYBOARD_H
 
 enum {
+  TD_F12_PW,
+  TD_F11_USR,
   TD_F10_NOFORMAT,
   TD_F9_CWXTITVAL
 };
+
+void td_f12_pw_func(qk_tap_dance_state_t *state, void *user_data) {
+      	if (state->count == 1) {
+	     	register_code16(KC_F12);
+	     	unregister_code16(KC_F12);
+      	} else {
+	     	SEND_STRING("xxx");
+	     	reset_tap_dance(state);
+      	}
+}  
+
+void td_f11_usr_func(qk_tap_dance_state_t *state, void *user_data) {
+      	if (state->count == 1) {
+	     	register_code16(KC_F11);
+	     	unregister_code16(KC_F11);
+      	} else {
+	     	SEND_STRING("xxx");
+	     	reset_tap_dance(state);
+      	}
+}
+
 
 void td_f10_noformat_func(qk_tap_dance_state_t *state, void *user_data) {
 	if (state->count == 1) {
@@ -60,13 +83,15 @@ void td_f9_cwxtitval_func(qk_tap_dance_state_t *state, void *user_data) {
 //Tap Dance definitions
 qk_tap_dance_action_t tap_dance_actions[] = {
 	[TD_F9_CWXTITVAL] = ACTION_TAP_DANCE_FN(td_f9_cwxtitval_func),
-	[TD_F10_NOFORMAT] = ACTION_TAP_DANCE_FN(td_f10_noformat_func)
+	[TD_F10_NOFORMAT] = ACTION_TAP_DANCE_FN(td_f10_noformat_func),
+        [TD_F11_USR] = ACTION_TAP_DANCE_FN(td_f11_usr_func),  
+        [TD_F12_PW] = ACTION_TAP_DANCE_FN(td_f12_pw_func)
 }; // comment
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_tkl_ansi(
-      KC_ESC,  KC_F1,  KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8,  TD(TD_F9_CWXTITVAL), TD(TD_F10_NOFORMAT), KC_F11,  KC_F12,                      KC_F16,  KC_F17,  KC_F19,
+      KC_ESC,  KC_F1,  KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8,  TD(TD_F9_CWXTITVAL), TD(TD_F10_NOFORMAT), TD(TD_F11_USR),   TD(TD_F12_PW),                      KC_F16,  KC_F17,  KC_F19,
       KC_GRV,  KC_1,   KC_2,  KC_3,  KC_4,  KC_5,  KC_6,  KC_7,  KC_8,   KC_9,                KC_0,                KC_MINS, KC_EQL,  KC_BSPC,            KC_INS,  KC_HOME, KC_PGUP,
       KC_TAB,  KC_Q,   KC_W,  KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,  KC_I,   KC_O,                KC_P,                KC_LBRC, KC_RBRC, KC_BSLS,            KC_DEL,  KC_END,  KC_PGDN,
       KC_CAPS, KC_A,   KC_S,  KC_D,  KC_F,  KC_G,  KC_H,  KC_J,  KC_K,   KC_L,                KC_SCLN,             KC_QUOT, KC_ENT,                                       
